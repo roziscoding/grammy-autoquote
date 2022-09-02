3e8172a87239c188e8cc7811e3c68b2888a84b12
Roz <roz@rjmunhoz.me>
2022-09-02 14:31:53 -0300

    chore: import grammy from lib.deno.dev

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,2 @@
 export * from "https://cdn.skypack.dev/@grammyjs/types@v2?dts";
-export * from "https://deno.land/x/grammy@v1.10.1/mod.ts";
+export * from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
