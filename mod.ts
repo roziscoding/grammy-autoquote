d3c0e6a7d910aa2b6d8879f8d85c09ac2ba79e80
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-06-20 14:33:50 +0530

    Fix mistake in documentation (#248510) (#5)

--- mod.ts
+++ mod.ts
@@ -21,7 +21,7 @@ export type AutoQuoteOptions = {
   /**
    * Mirror for the property described in the [documentation for ReplyParameters](https://core.telegram.org/bots/api#replyparameters)
    */
-  allowSendingWithoutReply: boolean;
+  allow_sending_without_reply: boolean;
 };
 
 /**
@@ -75,7 +75,7 @@ export function addReplyParam<C extends Context>(
           message_id: (payload as any).reply_parameters?.message_id ??
             ctx.msg?.message_id,
           chat_id: (payload as any).reply_parameters?.chat_id ?? ctx.chat?.id,
-          allow_sending_without_reply: options?.allowSendingWithoutReply,
+          allow_sending_without_reply: options?.allow_sending_without_reply,
         },
       },
       signal,
